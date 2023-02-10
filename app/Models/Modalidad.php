3c0b376f19f9cfc1077fa97f5dcd37b21dde213c
Ariel Rodrigo Colque Garcia <colqueariel023@gmail.com>
2023-02-10 19:59:38 -0400

    feat: Add relationship between modality and tournament

--- app/Models/Modalidad.php
+++ app/Models/Modalidad.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Modalidad extends Model
 {
     use HasFactory;
+
+    public function torneos()
+    {
+        return $this->hasMany(Torneo::class, 'modalidad_id', 'id');
+    }
 }
