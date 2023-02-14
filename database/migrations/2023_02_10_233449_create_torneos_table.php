6cf11eee63eecd6c7da821d2992c0feaa725ec30
Ariel Rodrigo Colque Garcia <colqueariel023@gmail.com>
2023-02-14 16:01:44 -0400

    feat: add the data in the Torneo table

--- database/migrations/2023_02_10_233449_create_torneos_table.php
+++ database/migrations/2023_02_10_233449_create_torneos_table.php
@@ -15,6 +15,17 @@ return new class extends Migration
     {
         Schema::create('torneos', function (Blueprint $table) {
             $table->id();
+            $table->string('nombre', 30);
+            $table->string('descripcion')->nullable();
+            $table->string('sede')->nullable();
+            $table->date('fecha_inicio');
+            $table->date('fecha_final');
+
+            // llave foranea, realacion de 1:N
+            $table->bigInteger('modalidad_id')->unsigned();
+            $table->bigInteger('categoria_id')->unsigned();
+            $table->foreign('modalida_id')->references('id')->on('modalidads');
+            $table->foreign('categoria_id')->references('id')->on('categorias');
             $table->timestamps();
         });
     }
