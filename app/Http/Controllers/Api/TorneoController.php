d978334fafbdfc9919f891cd51a334c9806c08ef
Ariel Rodrigo Colque Garcia <colqueariel023@gmail.com>
2023-02-14 19:37:02 -0400

    feat: add function index torneo

--- app/Http/Controllers/Api/TorneoController.php
+++ app/Http/Controllers/Api/TorneoController.php
@@ -3,9 +3,34 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Torneo;
 use Illuminate\Http\Request;
 
 class TorneoController extends Controller
 {
-    //
+    public function index()
+    {
+        $lista_torneo = Torneo::all();
+        return response()->json($lista_torneo, 200);
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show($id)
+    {
+        //
+    }
+
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    public function destroy($id)
+    {
+        //
+    }
 }
