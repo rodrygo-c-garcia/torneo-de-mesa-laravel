b804d09ac7d1927c86b68eb90d774d2edebc163a
Ariel Rodrigo Colque Garcia <colqueariel023@gmail.com>
2023-02-14 16:04:33 -0400

    feat: add the date in the Modalidad table

--- database/migrations/2023_02_10_232551_create_modalidads_table.php
+++ database/migrations/2023_02_10_232551_create_modalidads_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('modalidads', function (Blueprint $table) {
             $table->id();
+            $table->string('nombre')->nullable();
+            $table->text('descripcion');
             $table->timestamps();
         });
     }
