13e4ab0b1274bbbccd5a1e24ec9588586aa04888
Ariel Rodrigo Colque Garcia <colqueariel023@gmail.com>
2023-02-14 20:15:19 -0400

    feat: add function store Torneo

--- app/Http/Controllers/Api/TorneoController.php
+++ app/Http/Controllers/Api/TorneoController.php
@@ -16,7 +16,26 @@ class TorneoController extends Controller
 
     public function store(Request $request)
     {
-        //
+        // validar
+        $request->validate([
+            'nombre' => 'required|min:3|max:50|unique:torneos',
+        ]);
+
+        try {
+            $torneo = new Torneo();
+            $torneo->nombre = $request->nombre;
+            $torneo->descripcion = $request->descripcion;
+            $torneo->sede = $request->sede;
+            $torneo->fecha_inicio = $request->fecha_inicio;
+            $torneo->fecha_final = $request->fecha_final;
+            $torneo->categoria_id = $request->categoria_id;
+            $torneo->modalidad_id = $request->modalidad_id;
+            $torneo->save();
+
+            return response()->json(['mensaje' => 'Torneo registrado', 'data' => $torneo], 201);
+        } catch (\Exception $e) {
+            return response()->json(['mensaje' => 'Torneo NO registrado'], 400);
+        }
     }
 
     public function show($id)
